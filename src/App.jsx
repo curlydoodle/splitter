47a9df2e0fd63e8f516c3fc8a40cb7991b15f4a1
Marek Zelinka <mzelinka17@gmail.com>
2023-09-15 10:52:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => (
+  <div className="flex min-h-full flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Split bills with friends &amp; colleagues
+      </h2>
+    </div>
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+      <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12"></div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: colors.stone,
+        green: colors.emerald,
+      },
+    },
+  },
+  plugins: [],
+};
